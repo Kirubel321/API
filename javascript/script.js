166befb7f133f5cf9814653cc59c84ae9d489d81
Kirubel321 <72298742+Kirubel321@users.noreply.github.com>
2022-04-24 14:06:29 +0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,7 @@ const fimg = document.getElementById('image');
 const fchar = document.getElementById('charname');
 const fquote = document.getElementById('quote');
 const ftitle = document.getElementById('title_');
-const pic = ["https://im.indiatimes.in/content/2020/Mar/Webpnet-resizeimage-60_5e5ca3a06cc0d.jpg", "https://i.pinimg.com/736x/e8/d9/bd/e8d9bdc79401b3c4d52356ba1a69fea0.jpg", "https://i.pinimg.com/736x/06/db/fb/06dbfbe25e8512cd41fb1f188f4abfa9.jpg", "https://cdn11.bigcommerce.com/s-e4notjahj4/images/stencil/original/products/718/3321/AC24-MAY-Ross__68623.1582460285.jpg", "https://m.media-amazon.com/images/M/MV5BNTI1MTc1ODYwNl5BMl5BanBnXkFtZTgwMTcxMzYzMzE@._V1_.jpg"];
+const pic = ["https://im.indiatimes.in/content/2020/Mar/Webpnet-resizeimage-60_5e5ca3a06cc0d.jpg", "https://i.pinimg.com/736x/e8/d9/bd/e8d9bdc79401b3c4d52356ba1a69fea0.jpg", "https://i.pinimg.com/736x/06/db/fb/06dbfbe25e8512cd41fb1f188f4abfa9.jpg", "https://cdn11.bigcommerce.com/s-e4notjahj4/images/stencil/original/products/718/3321/AC24-MAY-Ross__68623.1582460285.jpg","https://i.pinimg.com/originals/0b/29/40/0b29407769b30531e2ffaec8d14f0244.jpg", "https://m.media-amazon.com/images/M/MV5BNTI1MTc1ODYwNl5BMl5BanBnXkFtZTgwMTcxMzYzMzE@._V1_.jpg"];
 
 async function init() {       
     const response = await fetch(url);
